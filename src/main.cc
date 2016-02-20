42554db5c2871933926c1d51d6215d3fd8e72575
Max Mertens <max.mail@dameweb.de>
2016-02-20 17:55:50 +0100

    Basic project structure.

---
+++ src/main.cc
@@ -0,0 +1,9 @@
+/**
+ * auc - The Aurum compiler
+ * Copyright (c) 2016  Max Mertens <max.mail@dameweb.de>
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
+ */
+
+int main() {
+    return 0;
+}
